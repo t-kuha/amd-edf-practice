76e62fad2351fc97bcfd23d535f32f6e04cb1c11
t-kuha <imagingtechnerd@gmail.com>
2025-11-29 11:20:46 +0000

    Update initialization script

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
+# stop on error
+set -euo pipefail
+
 # google repo tool
-mkdir -p ~/.bin
+if [ ! -d "${HOME}/.bin" ]; then
+    mkdir -p ~/.bin
+    curl https://storage.googleapis.com/git-repo-downloads/repo > ~/.bin/repo
+    chmod a+rx ~/.bin/repo
+fi
 PATH="${HOME}/.bin:${PATH}"
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/.bin/repo
-chmod a+rx ~/.bin/repo
 
 # necessary packages
-sudo apt update
-sudo apt install chrpath cpio diffstat zstd
+sudo apt -y update
+sudo apt -y install chrpath cpio diffstat zstd
+
+# get sources
+mkdir -p yocto/edf
+pushd yocto/edf
+repo init -u https://github.com/Xilinx/yocto-manifests.git -b rel-v2025.2 -m default-edf.xml
+repo sync
+popd
+
+echo "setup complete."
