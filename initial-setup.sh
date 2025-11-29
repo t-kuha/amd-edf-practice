fbb0cefdac7dd684b18f29c7103a1e35187f307a
t-kuha <imagingtechnerd@gmail.com>
2025-11-29 01:59:08 +0000

    Add apt install

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# google repo tool
 mkdir -p ~/.bin
 PATH="${HOME}/.bin:${PATH}"
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/.bin/repo
 chmod a+rx ~/.bin/repo
+
+# necessary packages
+sudo apt update
+sudo apt install chrpath cpio diffstat zstd
