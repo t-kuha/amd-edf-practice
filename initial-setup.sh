08fbbfad3efe7ac2b7a006d3288c943b6abfb55b
t-kuha <imagingtechnerd@gmail.com>
2025-11-30 17:14:20 +0900

    Add required packages

--- initial-setup.sh
+++ initial-setup.sh
@@ -13,7 +13,9 @@ PATH="${HOME}/.bin:${PATH}"
 
 # necessary packages
 sudo apt -y update
-sudo apt -y install chrpath cpio diffstat zstd
+sudo apt -y install build-essential chrpath cpio debianutils diffstat file gawk gcc git \
+iputils-ping libacl1 liblz4-tool locales python3 python3-git python3-jinja2 python3-pexpect \
+python3-pip python3-subunit socat texinfo unzip wget xz-utils zstd
 
 # get sources
 mkdir -p yocto/edf
