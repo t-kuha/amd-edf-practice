85059541e40b4014bf7c94a3c2c9b64a7e6b717a
t-kuha <imagingtechnerd@gmail.com>
2026-01-12 05:40:17 +0000

    Add tmux as dependency

--- initial-setup.sh
+++ initial-setup.sh
@@ -15,7 +15,7 @@ PATH="${HOME}/.bin:${PATH}"
 sudo apt -y update
 sudo apt -y install build-essential chrpath cpio debianutils diffstat file gawk gcc git \
 iputils-ping libacl1 liblz4-tool locales python3 python3-git python3-jinja2 python3-pexpect \
-python3-pip python3-subunit socat texinfo unzip wget xz-utils zstd
+python3-pip python3-subunit socat texinfo tmux unzip wget xz-utils zstd
 
 # get sources
 CWD=$(pwd)
